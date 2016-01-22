0d471044668102f07b77c2712c9cbf7bb0d9fa2d
Devin Pastoor <devin.pastoor@gmail.com>
2016-01-21 22:05:24 -0800

    ahhhhh module name can't be the same as file name dummy

--- dl_data.py
+++ dl_data.py
@@ -7,31 +7,42 @@ download_urls = []
 BASE = 'http://data.githubarchive.org/'
 year = 2015
 archive_2015 = []
-for mon in range(1, 13):
+for mon in range(6, 13):
     days = calendar.monthrange(year,mon)[1]
     month = str(mon) if mon > 9 else "0" + str(mon)
     for day in range(1, days+1):
         day_str = str(day) if day > 9 else "0" + str(day)
         for hour in range(1, 24):
             download_urls.append(str(BASE) + str(year) + "-" + month + "-" + day_str + "-" + str(hour) + ".json.gz")
-        
+ 
+mon = 1
+year = 2016
+days = 20
+month = str(mon) if mon > 9 else "0" + str(mon)
+for day in range(1, days+1):
+    day_str = str(day) if day > 9 else "0" + str(day)
+    for hour in range(1, 24):
+        dl_url = str(BASE) + str(year) + "-" + str(month) + "-" + day_str + "-" + str(hour) + ".json.gz"
+        download_urls.append(dl_url)      
+
 
 
 # In[48]:
-
 def wget_github(url):
     try:
         filename = wget.download(url)
-    except:
+    except Exception as ex:
+        template = "An exception of type {0} occured. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        print(message)
         with open("wget_log.txt", "a") as log:
-            log.write("ERROR IN: " + url + "\n")
+            log.write("ERROR IN: " + url + " with message: \n")
+            log.write(message)
+            log.write("\n")
             log.close()
         
 
 
-# In[49]:
-
-wget_github(download_urls[0])
 
 
 # In[53]:
